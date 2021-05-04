f778fc19e164904080a1f001a6f6aed1a2ef0bc4
MiraElise <70745409+MiraElise@users.noreply.github.com>
2021-05-04 17:12:42 +0200

    script
    script

---
+++ It's on/script.js
@@ -0,0 +1,102 @@
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * 1) USEFULL STUFF  
+ */
+
+    // - - - DOM ELEMENTS - - -
+        // The city select element that triggers the event
+        const citySelectorInput = document.querySelector('.citySelector__input');
+
+        // The temperature element
+        const wind = document.querySelector('.weatherDisplay__wind');
+
+
+        // The city name
+        const city = document.querySelector('.weatherDisplay__city');
+
+    
+
+
+    // - - - API VARIABLES - - - 
+
+        // The URL used for the API call (example of full URL: https://vejr.eu/api.php?location=Sonderborg&degree=C)
+        const apiBaseUrl = 'https://vejr.eu/api.php';
+
+        // The location option, setting the city to get weather data for
+        let apiLocation = 'Vejen';
+
+        // The degree option, setting the temperature scale to use
+        let apiDegree = 'c';
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * 2) RUN ON LOAD  
+ */
+
+loadData();
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * 3) EVENTLISTENERS  
+ */
+
+    citySelectorInput.addEventListener('change', changeCity);
+
+
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+ * 4) FUNCTIONS  
+ */
+
+    // Change the selected city, when user selects a new city
+    function changeCity() {
+        console.log('Change the city from', apiLocation);
+        // update to the newly selected location
+        apiLocation = citySelectorInput.value;
+        
+        console.log('to', apiLocation);
+        loadData();
+    }
+
+
+    // Loads the data from the API (Notice: Magic is happening here!)
+    function loadData() {
+        let url = generateApiFullString();
+        console.log('Calling the API with the following URL:',url);
+        fetch(url)
+            .then(response => response.json())
+            .then(data => updateHtml(data));
+    }
+
+
+    function updateHtml(data) {
+        console.log('received the following data:',data);
+
+        // update name of the city with the currently selected one
+        city.textContent = data.LocationName;
+        // Først hent data fra vejr api, er gemt i variablen data
+        let windDataString = data.CurrentData.windText;
+         // f.ek.s "4 m/s Vest"
+// Split string til et array, split på mellemrum " "
+let windDataArray = windDataString.split(" "); 
+// Konverter første del af array fra string til tal, f.eks. "4" til 4
+let currentWind = parseInt(windDataArray[0]);
+// Nu kan wind bruges til comparison, f.eks. wind>20
+        // update the temperature
+        wind.textContent = currentWind;
+    }
+
+
+    // Returns the full API URL string to send the GET request with
+    function generateApiFullString() {
+        return apiBaseUrl + "?location=" + apiLocation + "&degree=" + apiDegree;
+    }
+
+
+
+
+
+
+// The citySelector <section> enabling the user to select a city
+const citySelectorSection = document.querySelector('.citySelector');
+
+// The weatherDisplay <section> that displays the weather data for the chosen city
+const weatherDisplaySection = document.querySelector('.weatherDisplay');
